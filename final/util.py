3fd426782da870b45a868a481ffd730e66cc4a63
Asmi K <168493757+asmik12@users.noreply.github.com>
2024-11-25 00:12:27 +0000

    fixed recommendation function

---
+++ final/util.py
@@ -0,0 +1,19 @@
+#Utility file to collect all the functions defined for the generating recommendations functionality.
+
+
+#Checks if a user exits in a dataframe
+def check_user_exists(users_df, user_id):
+    if user_id in users_df['user_id'].values:
+        print(f"User ID {user_id} exists in the DataFrame.")
+        return True
+    else:
+        print(f"User ID {user_id} does not exist in the DataFrame.")
+        return False
+
+
+#Given a product id, looks up the product name in the original dataframe and returns it
+def lookup_product_name(product_ids, original_products):
+    for id in product_ids:
+        product_row = original_products[original_products['id'] == id]
+        name = product_row['name']
+        print(f"ID = {id}, Name = {name}")
